e60c8d6361cc740640d6e6bd346eeebacc41cfc7
Kim Fui Leung <93153597+isaackk60@users.noreply.github.com>
2024-02-27 14:24:36 +0000

    get merge

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,17 +1,19 @@
-import React, { Component } from "react"
-import { Link } from "react-router-dom"
+import React, {Component} from "react"
+import {Link} from "react-router-dom"
 
 import axios from "axios"
 import NavigationBar from "./NavigationBar"
 import ShirtBlock from "./ShirtBlock"
 
-import { ACCESS_LEVEL_GUEST, ACCESS_LEVEL_ADMIN, SERVER_HOST } from "../config/global_constants"
+import {ACCESS_LEVEL_GUEST, ACCESS_LEVEL_ADMIN, SERVER_HOST} from "../config/global_constants"
 
 
-export default class Main extends Component {
-    constructor(props) {
+export default class Main extends Component 
+{
+    constructor(props) 
+    {
         super(props)
-
+        
         this.state = {
             shirts:[],
             searchQuery:"",
@@ -19,9 +21,10 @@ export default class Main extends Component {
             genderFilter: "",
         }
     }
-
-
-    componentDidMount() {
+    
+    
+    componentDidMount() 
+    {
         axios.get(`${SERVER_HOST}/shirts`)
         .then(res => 
         { 
@@ -31,16 +34,47 @@ export default class Main extends Component {
         {
             // do nothing
         })
-        console.log(this.state.shirts)
+
     }
 
+    handleSearchInputChange = (event) => {
+        this.setState({ searchQuery: event.target.value });
+    };
+
+    handleSortChange = event => {
+        this.setState({ sortType: event.target.value });
+    };
+
+    handleGenderFilterChange = (event) => {
+        this.setState({ genderFilter: event.target.value });
+    };
   
     render() 
     {   
-        return (           
+        const { shirts, searchQuery,sortType,genderFilter } = this.state;
+        let filteredShirtsName = shirts.filter(shirt =>
+            shirt.name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+        if (sortType === "PriceHighToLow") {
+            filteredShirtsName.sort((a, b) => b.price - a.price);
+        } else if (sortType === "PriceLowToHigh") {
+            filteredShirtsName.sort((a, b) => a.price - b.price);
+        }
+
+
+        if (genderFilter === "male") {
+            filteredShirtsName = filteredShirtsName.filter(shirt => shirt.gender === "male");
+        } else if (genderFilter === "female") {
+            filteredShirtsName = filteredShirtsName.filter(shirt => shirt.gender === "female");
+        } else if (genderFilter === "unisex") {
+            filteredShirtsName = filteredShirtsName.filter(shirt => shirt.gender === "unisex");
+        }
+            return (           
             <div>
-                <NavigationBar />
+                <NavigationBar genderFilter={genderFilter}  handleGenderFilterChange={this.handleGenderFilterChange.bind(this)}/>
+                <div className="wholeMain">
                 {
+                    
                     // localStorage.accessLevel > ACCESS_LEVEL_GUEST 
                     // ? <div className="logout">
                     //     {
@@ -55,10 +89,32 @@ export default class Main extends Component {
                     //     <Link className="blue-button" to={"/Register"}>Register</Link>  
                     //     <Link className="red-button" to={"/ResetDatabase"}>Reset Database</Link>  <br/><br/><br/></div>
                 }
+
+                <div className="search-container">
+                    <input type="text"  placeholder="Search shirts..." value={searchQuery} onChange={this.handleSearchInputChange} />
+                </div>
+
+
+                <div className="sort-container">
+                    <select value={sortType} onChange={this.handleSortChange}>
+                        <option value="default">Default Sorting</option>
+                        <option value="PriceHighToLow">Price: High to Low</option>
+                        <option value="PriceLowToHigh">Price: Low to High</option>
+                    </select>
+                    {/* <select value={genderFilter} onChange={this.handleGenderFilterChange}>
+                        <option value="">All Genders</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        <option value="unisex">Unisex</option>
+                    </select> */}
+                </div>
+
+                
                 
                 <div className="main-container">
                 
-                    {this.state.shirts.map((shirt)=><ShirtBlock key={shirt._id} shirt={shirt} />)} 
+                    {/* {this.state.shirts.map((shirt)=><ShirtBlock key={shirt._id} shirt={shirt} />)}  */}
+                    {filteredShirtsName.map((shirt) => <ShirtBlock key={shirt._id} shirt={shirt} gender={shirt.gender}/>)}
                     
                 </div>
                 {
@@ -69,7 +125,9 @@ export default class Main extends Component {
                           </div>
                         : null
                     }
+                    
             </div> 
+            </div>
             
         )
     }
--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -1,138 +1,38 @@
-// import React, { Component } from "react";
+// import React, { Component } from "react"
 // import Form from "react-bootstrap/Form"
-
-// import { Redirect } from "react-router-dom"
-// import { SANDBOX_CLIENT_ID, SERVER_HOST } from "../config/global_constants"
-// import PayPalMessage from "./PayPalMessage"
-// import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js"
-
-// import NavigationBar from "./NavigationBar"
-
 
 // import axios from "axios"
 
 // export default class ShoppingCart extends Component {
-//     constructor(props) {
-//         super(props)
-
+//     constructor(props){
 //         this.state = {
-//             redirectToPayPalMessage: false,
-//             payPalMessageType: null,
-//             payPalOrderID: null,
-//             totalPrice: 0
-//         }
+//             items: [], 
+//             totalPrice: 0 
+//         };
 //     }
 
-//     componentDidMount() {
-//         const { itemsInCart } = this.props.location.state || { itemsInCart: [] };
-//         let totalPrice = 0;
-//         itemsInCart.forEach((item) => {
-//           totalPrice += item.price * item.quantity;
-//         });
-//         this.setState({ totalPrice }); // Update totalPrice in state
-//       }
-
-//     //   createOrder = (data, actions) => {
-//     //     // Return an object with the correct amount based on the total price
-//     //     return actions.order.create({
-//     //       purchase_units: [
-//     //         {
-//     //           amount: {
-//     //             value: this.state.totalPrice, // Use the total price from state
-//     //             currency_code: "EUR",
-//     //           },
-//     //         },
-//     //       ],
-//     //     });
-//     //   };
-
-//     //   createOrder = (data, actions) => 
-//     //   {
-//     //       return actions.order.create({purchase_units:[{amount:{value:this.props.price}}]})
-//     //   }
-
-//     createOrder = (data, actions) => {
-//         const { itemsInCart } = this.props.location.state || { itemsInCart: [] };
-//         let totalPrice = 0;
-//         itemsInCart.forEach((item) => {
-//           totalPrice += item.price * item.quantity;
-//         });
-//         return actions.order.create({
-//           purchase_units: [
-//             {
-//               amount: {
-//                 value: totalPrice.toFixed(2), // Ensure total price is formatted properly
-//                 currency_code: "EUR",
-//               },
-//             },
-//           ],
-//         });
-//       };
-      
-      
-//       onApprove = paymentData =>
-//       {      
-//           axios.post(`${SERVER_HOST}/sales/${paymentData.orderID}/${this.props.shirtID}/${this.props.price}`, {headers:{"authorization":localStorage.token, "Content-type": "multipart/form-data"}})
-//           .then(res => 
-//           {                   
-//               this.setState({payPalMessageType:PayPalMessage.messageType.SUCCESS, 
-//                              payPalOrderID:paymentData.orderID, 
-//                              redirectToPayPalMessage:true}) 
-//           })
-//           .catch(errorData =>
-//           {           
-//               this.setState({payPalMessageType:PayPalMessage.messageType.ERROR, 
-//                              redirectToPayPalMessage:true}) 
-//           })
-//       }
-   
-          
-//       onError = errorData => 
-//       {
-//           this.setState({payPalMessageType:PayPalMessage.messageType.ERROR, 
-//                          redirectToPayPalMessage:true})         
-//       }
-      
-      
-//       onCancel = cancelData => 
-//       {
-//           // The user pressed the Paypal checkout popup window cancel button or closed the Paypal checkout popup window
-//           this.setState({payPalMessageType:PayPalMessage.messageType.CANCEL, 
-//                          redirectToPayPalMessage:true})       
-//       }
-
+//     addItemToCart = (item) => {
+//         const updatedItems = [...this.state.items, item];
+//         const totalPrice = this.state.totalPrice + (item.price * item.quantity);
+//         this.setState({ items: updatedItems, totalPrice });
+//     };
 
 //     render() {
-//         const { itemsInCart } = this.props.location.state || { itemsInCart: [] };
-
-//         // let totalPrice = 0;
-//         // itemsInCart.forEach(item => {
-//         //     totalPrice += item.price * item.quantity;
-//         // });
-
-
+//         const { items, totalPrice } = this.state;
+//         const { itemsInCart } = this.props.location.state;
 
 //         return (
 //             <div>
-//                 <NavigationBar />
 //                 <h2>Shopping Cart</h2>
 //                 <ul>
-//                     {itemsInCart.map((item, index) => (
+//                     {items.map((item, index) => (
 //                         <li key={index}>
-//                             {item.name} - Quantity: {item.quantity} - Price: €{item.price * item.quantity}
+//                             {item.name} - Quantity: {item.quantity} - Price: ${item.price * item.quantity}
 //                         </li>
 //                     ))}
 //                 </ul>
-//                 <p>Total Price: €{this.state.totalPrice}</p>
-//                 <div>
-//                     {this.state.redirectToPayPalMessage ? <Redirect to={`/PayPalMessage/${this.state.payPalMessageType}/${this.state.payPalOrderID}`} /> : null}
-
-//                     <PayPalScriptProvider options={{ currency: "EUR", "client-id": SANDBOX_CLIENT_ID }}>
-//                         <PayPalButtons style={{ layout: "horizontal" }} createOrder={this.createOrder} onApprove={this.onApprove} onError={this.onError} onCancel={this.onCancel} />
-//                     </PayPalScriptProvider>
-//                 </div>
+//                 <p>Total Price: ${totalPrice}</p>
 //             </div>
-            
 //         );
 //     }
 // }
@@ -381,14 +281,6 @@ export default class ShoppingCart extends Component {
         // // Update totalPrice state
         // this.setState({ totalPrice: totalPrice });
         this.state.cart.map(item => {
-        // Calculate total price based on cart items
-        // this.state.cart.forEach(item => {
-        //     const carItem=item.cartItems[0];
-        //     totalPrice += carItem.price * carItem.quantity;
-        // });
-        // // Update totalPrice state
-        // this.setState({ totalPrice: totalPrice });
-        this.state.cart.map(item => {
             totalPrice += item.price * item.quantity;
         });
         // Update totalPrice state
@@ -506,10 +398,8 @@ export default class ShoppingCart extends Component {
                 {this.state.cart !== undefined ? <p>Total Price: {this.calculateTotalPrice()}</p> : null}
 
             </div>
-            
         );
     }
 
 }
-
 
